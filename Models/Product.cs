e522fa4c3d0fa0517d0407da4b62ceb074220360
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2023-08-12 12:56:36 +0500

    added models

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreWebAPI_ClientSide.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string? Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
